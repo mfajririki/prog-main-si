fbde5df9b1a9c010f7f1fc9b4d2eb20e9e51ee2b
mfajririki <fajririki07@gmail.com>
2021-12-15 15:29:25 +0700

    updated

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -200,7 +200,9 @@
                                 <div class="gdlr-core-pbf-element">
                                     <div class="gdlr-core-image-item gdlr-core-item-pdb  gdlr-core-center-align gdlr-core-item-pdlr">
                                         <div class="gdlr-core-image-item-wrap gdlr-core-media-image  gdlr-core-image-item-style-rectangle" style="border-width: 0px;">
+                                            @foreach ($profile_kaprodi as $kaprodi)
                                             <img src="{{ $kaprodi->photo }}" width="450px" height="450px" />
+                                            @endforeach                                            
                                         </div>
                                     </div>
                                 </div>
